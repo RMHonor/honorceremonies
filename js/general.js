388a9c884c3eb1088508f001e7dac05f8dc7fe24
Richard Honor <me@r-honor.uk>
2016-06-15 19:09:10 +0100

    Landing image updated. General fixes

--- js/general.js
+++ js/general.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 	
 	//navbar click events
-    $("nav a[href^='#']").on("click", function(e){
+    $("nav a[href^='#']").on('click', function(e){
 		//prevent immediate jump followed by animation
 		e.preventDefault();
 		
 		//change active link
-		$(".nav").find(".active").removeClass("active");
-		$(this).parent().addClass("active");
+		$(".nav").find('.active').removeClass('active');
+		$(this).parent().addClass('active');
 
 		//get target
 		var hash = this.hash;
@@ -25,19 +25,46 @@ $(document).ready(function(){
 	});
 	
 	//contact form events
-	$input = $(".contact-form input, .contact-form textarea")
-	$input.on("input", function(){
+	$input = $('.contact-form input, .contact-form textarea')
+	$input.on('input', function(){
 		var empty = false;
 		$input.each(function() {
 			if(this.value == ""){
 				empty = true;
-				return;
 			}
 		});
 		if (!empty){
-			$(".contact-btn").removeAttr("disabled");
+			$('.contact-btn').removeAttr('disabled');
 		} else {
-			$(".contact-btn").attr("disabled");
+			$('.contact-btn').attr('disabled');
+		}
+	});
+	
+	$('.contact-btn').on('click', function(){
+		var empty = false;
+		$input.each(function() {
+			if(this.value == ""){
+				empty = true;
+			}
+		});
+		
+		if (!empty){
+			var name = $('.client-name').val(),
+				email = $('.client-email').val(),
+				message =  $('.client-message').val();
+			$.post("php/mail.php", {
+				'from': name,
+				'email': email,
+				'message': message
+			})
+			.done(function(a, b, xhr){
+				if (xhr.status == 200){
+					alert("Success")
+				}
+			})
+			.fail(function(xhr, status, error){
+				alert("Error: " + error);
+			});
 		}
 	});
 });
